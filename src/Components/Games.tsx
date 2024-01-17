78f853204de39bfd0c49d8939a348a7d821f70a0
aw3s0m3ss <aw3s0m3ss@gmail.com>
2024-01-17 21:46:09 +0200

    quick fix

--- src/Components/Games.tsx
+++ src/Components/Games.tsx
@@ -5,11 +5,11 @@ export function Games() {
         <div className="page-container">
             <h1>Games That i made :</h1>
             <div className="games-container">
-                <div className="game-2">
                 <div className="game-6">
                     <h1>Break Out Game</h1>
                     <iframe title="Spirit Dog" src="https://dmitristark.github.io/game_003//" width="100%" height="100%" />
                 </div>
+                <div className="game-2">
                     <h1>Hunt The Ace Game</h1>
                     <iframe title="Tic Tak Game" src="https://dmitristark.github.io/Hunt_the_ace_game/" width="100%" height="100%" />
                 </div>
