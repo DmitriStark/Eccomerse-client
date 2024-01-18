4ac2fde24d801715a36daa0f38bfa4c20205bf87
aw3s0m3ss <aw3s0m3ss@gmail.com>
2024-01-18 21:15:41 +0200

    add image to footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -5,6 +5,9 @@ export function Footer() {
     return (
         <div className="footer">
             <p>© 2022 - All rights Reserved To Me Dmitri Stark</p>
+            <img src="../../../vite.png" alt="Footer Image" />
+            
         </div>
     )
 }
+
