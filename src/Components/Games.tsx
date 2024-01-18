a47222db23a078b5c9c902ec28f24d8818cf1d12
aw3s0m3ss <aw3s0m3ss@gmail.com>
2024-01-18 08:41:20 +0200

    updated games resolution

--- src/Components/Games.tsx
+++ src/Components/Games.tsx
@@ -7,31 +7,31 @@ export function Games() {
             <div className="games-container">
                 <div className="game-6">
                     <h1>Break Out Game</h1>
-                    <iframe title="Spirit Dog" src="https://dmitristark.github.io/game_003//" width="100%" height="100%" />
+                    <iframe title="Spirit Dog" src="https://dmitristark.github.io/game_003//" />
                 </div>
                 <div className="game-2">
                     <h1>Hunt The Ace Game</h1>
-                    <iframe title="Tic Tak Game" src="https://dmitristark.github.io/Hunt_the_ace_game/" width="100%" height="100%" />
+                    <iframe title="Tic Tak Game" src="https://dmitristark.github.io/Hunt_the_ace_game/" />
                 </div>
                 <div className="game-3">
                     <h1>Fighting Game</h1>
-                    <iframe title="Tic Tak Game" src="https://dmitristark.github.io/Figthing_Game/" width="100%" height="100%" />
+                    <iframe title="Tic Tak Game" src="https://dmitristark.github.io/Figthing_Game/"  />
                 </div>
                 <div className="game-1">
                     <h1>Tic Tak Game</h1>
-                    <iframe title="Tic Tak Game" src="https://dmitristark.github.io/tic-tac-Game/" width="100%" height="100%" />
+                    <iframe title="Tic Tak Game" src="https://dmitristark.github.io/tic-tac-Game/"  />
                 </div>
                 <div className="game-4">
                     <h1>Break Out Game</h1>
-                    <iframe title="Tic Tak Game" src="https://dmitristark.github.io/Break_Out_game/" width="100%" height="100%" />
+                    <iframe title="Tic Tak Game" src="https://dmitristark.github.io/Break_Out_game/"  />
                 </div>
                 <div className="game-5">
                     <h1>Break Out Game</h1>
-                    <iframe title="Tic Tak Game" src="https://dmitristark.github.io/Slide_Puzzle_Game/" width="100%" height="100%" />
+                    <iframe title="Tic Tak Game" src="https://dmitristark.github.io/Slide_Puzzle_Game/" />
                 </div>
                 <div className="game-5">
                     <h1>Legendarry Snake</h1>
-                    <iframe title="Tic Tak Game" src="https://dmitristark.github.io/snake/" width="100%" height="100%" />
+                    <iframe title="Tic Tak Game" src="https://dmitristark.github.io/snake/" />
                 </div>
             </div>
         </div>
