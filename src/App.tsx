35e6abc882e7803384be5517a61fc9dcc9ab704c
aw3s0m3ss <aw3s0m3ss@gmail.com>
2024-01-23 23:34:01 +0200

    css games and added header

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,25 @@ import NavBar from "./Components/Navbar";
 import { Lessons } from "./Components/Lessons";
 import { Games } from "./Components/Games";
 import { Footer } from "./Components/Footer";
-import {LocalStorageDemo} from "./Components/localStorage"
-
-
-
-
+import { LocalStorageDemo } from "./Components/localStorage";
+import { Header } from "./Components/Header";
 
 export default function App() {
-  return <>
-    <NavBar/>  
-    <div className="main">
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/lessons' element={<Lessons />} />
-      <Route path='/products' element={<Catalog />} />
-      <Route path='/products/:id' element={<ProductPage />} />
-      <Route path='/games' element={<Games />} />
-      <Route path='/localstorage' element={<LocalStorageDemo />} />
-    </Routes>
-    </div>
-    <Footer/>
-  </>
+  return (
+    <>
+      <Header />
+      <NavBar />
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/lessons" element={<Lessons />} />
+          <Route path="/products" element={<Catalog />} />
+          <Route path="/products/:id" element={<ProductPage />} />
+          <Route path="/games" element={<Games />} />
+          <Route path="/localstorage" element={<LocalStorageDemo />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
 }
-
-
-
-
-
-
-
---
+++ src/Components/Header.tsx
@@ -0,0 +1,7 @@
+export function Header(){
+    return<>
+    <div className="header">
+        <h1>This is the header</h1>
+    </div>
+    </>
+}
