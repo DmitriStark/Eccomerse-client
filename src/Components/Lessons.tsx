60e51f96bf36ffd9d8a8d8bf2cc6be79acaddefc
aw3s0m3ss <aw3s0m3ss@gmail.com>
2024-01-27 19:41:29 +0200

    add more immages

--- src/Components/Lessons.tsx
+++ src/Components/Lessons.tsx
@@ -1,13 +1,21 @@
-
-
 export function Lessons() {
-    return (
-        <div>
-            <h1>Lessons</h1>
-            <img src="/images/vite.png" alt="Footer Image" />
-            <img src="/images/vite.png" alt="Footer Image" />
-            <img src="/images/vite.png" alt="Footer Image" />
-            <img src="/images/vite.png" alt="Footer Image" />
-        </div>
-    )
+  return (
+    <div>
+      <h1>Lessons</h1>
+      <img src="/images/vite.png" alt="Footer Image" />
+      <img src="/images/vite.png" alt="Footer Image" />
+      <img src="/images/vite.png" alt="Footer Image" />
+      <img src="/images/vite.png" alt="Footer Image" />
+      <img src="/images/vite.png" alt="Footer Image" />
+      <img src="/images/vite.png" alt="Footer Image" />
+      <img src="/images/vite.png" alt="Footer Image" />
+      <img src="/images/vite.png" alt="Footer Image" />
+      <img src="/images/vite.png" alt="Footer Image" />
+      <img src="/images/vite.png" alt="Footer Image" />
+      <img src="/images/vite.png" alt="Footer Image" />
+      <img src="/images/vite.png" alt="Footer Image" />
+      <img src="/images/vite.png" alt="Footer Image" />
+      <img src="/images/vite.png" alt="Footer Image" />
+    </div>
+  );
 }
