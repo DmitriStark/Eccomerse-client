18ecda146ef0a1e9f20b79a8bdfd0aa63d2e6ac9
aw3s0m3ss <aw3s0m3ss@gmail.com>
2024-01-30 17:19:33 +0200

    added more info to games

--- src/Components/Games.tsx
+++ src/Components/Games.tsx
@@ -8,30 +8,51 @@ export function Games() {
                 <div className="game-6">
                     <h1>Break Out Game</h1>
                     <iframe title="Spirit Dog" src="https://dmitristark.github.io/game_003//" />
+                    <p>deploy : <a href="https://dmitristark.github.io/game_003/"></a>link to full game</p>
+                    <p>repo : <a href="https://github.com/DmitriStark/game_003"></a>link to repo</p>
+                    <p>description made this game for practice and better understand mechanics of html css and javascript</p>
                 </div>
                 <div className="game-2">
                     <h1>Hunt The Ace Game</h1>
                     <iframe title="Tic Tak Game" src="https://dmitristark.github.io/Hunt_the_ace_game/" />
+                    <p>deploy : <a href="https://dmitristark.github.io/Hunt_the_ace_game/"></a>link to full game</p>
+                    <p>repo : <a href="https://github.com/DmitriStark/Hunt_the_ace_game"></a>link to repo</p>
+                    <p>description made this game for practice and better understand mechanics of html css and javascript</p>
                 </div>
                 <div className="game-3">
                     <h1>Fighting Game</h1>
                     <iframe title="Tic Tak Game" src="https://dmitristark.github.io/Figthing_Game/"  />
+                    <p>deploy : <a href="https://dmitristark.github.io/Figthing_Game/"></a>link to full game</p>
+                    <p>repo : <a href="https://github.com/DmitriStark/Figthing_Game"></a>link to repo</p>
+                    <p>description made this game for practice and better understand mechanics of html css and javascript</p>
                 </div>
                 <div className="game-1">
                     <h1>Tic Tak Game</h1>
                     <iframe title="Tic Tak Game" src="https://dmitristark.github.io/tic-tac-Game/"  />
+                    <p>deploy : <a href="https://dmitristark.github.io/tic-tac-Game/"></a>link to full game</p>
+                    <p>repo : <a href="https://github.com/DmitriStark/tic-tac-Game"></a>link to repo</p>
+                    <p>description made this game for practice and better understand mechanics of html css</p>
                 </div>
                 <div className="game-4">
                     <h1>Break Out Game</h1>
                     <iframe title="Tic Tak Game" src="https://dmitristark.github.io/Break_Out_game/"  />
+                    <p>deploy : <a href="https://dmitristark.github.io/Break_Out_game/"></a>link to full game</p>
+                    <p>repo : <a href="https://github.com/DmitriStark/Break_Out_game"></a>link to repo</p>
+                    <p>description made this game for practice and better understand mechanics of html css and javascript</p>
                 </div>
                 <div className="game-5">
                     <h1>Break Out Game</h1>
                     <iframe title="Tic Tak Game" src="https://dmitristark.github.io/Slide_Puzzle_Game/" />
+                    <p>deploy : <a href="https://dmitristark.github.io/Slide_Puzzle_Game/"></a>link to full game</p>
+                    <p>repo : <a href="https://github.com/DmitriStark/Slide_Puzzle_Game"></a>link to repo</p>
+                    <p>description made this game for practice and better understand mechanics of html css</p>
                 </div>
                 <div className="game-5">
                     <h1>Legendarry Snake</h1>
                     <iframe title="Tic Tak Game" src="https://dmitristark.github.io/snake/" />
+                    <p>deploy : <a href="https://dmitristark.github.io/snake/"></a>link to full game</p>
+                    <p>repo : <a href="https://github.com/DmitriStark/snake"></a>link to repo</p>
+                    <p>description made this game for practice and better understand mechanics of html css and javascript</p>
                 </div>
             </div>
         </div>
