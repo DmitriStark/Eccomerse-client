72b67cbb36532e4bc9759565229ea3b097d3006c
aw3s0m3ss <aw3s0m3ss@gmail.com>
2024-01-31 10:09:40 +0200

    added pic to ihome

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,8 +1,16 @@
-export function  Home() {
+import "../css/Home.css";
+
+export function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home-container">
+      <div className="me-container">
+        <img src="images/mewobackground.png" alt="" />
+      </div>
+      <div className="obout">
+        <h1>Its Me ╰(*°▽°*)╯</h1>
+        <p>my github <a href="https://github.com/DmitriStark">Click here</a></p>
+        <p>my youtube <a href="https://www.youtube.com/channel/UCibMun2cCSrO_sC9cy36lUQ">Click here</a></p>
+      </div>
     </div>
   );
 }
-    
