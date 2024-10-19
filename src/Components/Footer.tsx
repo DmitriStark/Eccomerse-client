9af317de504f793a14c56675ec068948d02e7170
aw3s0m3ss <aw3s0m3ss@gmail.com>
2024-10-19 14:29:16 +0300

    just fix year in footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -5,7 +5,7 @@ import "../css/footer.css"
 export function Footer() {
     return (
         <div className="footer">
-            <p>© 2022 - All rights Reserved To Me Dmitri Stark</p>
+            <p>© 2024 - All rights Reserved To Me Dmitri Stark</p>
             <img src="/images/vite.png" alt="Footer Image" />
             
         </div>
